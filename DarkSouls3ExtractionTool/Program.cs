60ab71ac3e83ab7634ccd59ae9f3a281dfe65fb5
Pireax <pim.remkes@gmail.com>
2016-05-17 22:14:31 +0200

    Added file offsets for each row. No longer shows unknown values by default. Added support for Magic, Accessorie, Protector and NpcParam .param files. Included more missing fields.

--- DarkSouls3ExtractionTool/Program.cs
+++ DarkSouls3ExtractionTool/Program.cs
@@ -11,7 +11,14 @@ namespace DarkSouls3ExtractionTool
 {
     class Program
     {
-        public static bool headers;
+        public enum Arguments
+        {
+            None        = 0x0, // 0b000
+            Headers     = 0x1, // 0b001
+            HeadersPlus = 0x3, // 0b011
+            Debug       = 0x4, // 0b100
+        }
+        public static Arguments arguments = Arguments.None;
 
         public static T BytesToStruct<T>(byte[] value, int startIndex) where T : struct
         {
@@ -26,12 +33,29 @@ namespace DarkSouls3ExtractionTool
             return structure;
         }
 
+        public static bool ShouldSkip(Type fieldType, Type typeToSkip)
+        {
+            if ((arguments & Arguments.Debug) == Arguments.Debug)
+                return false;
+
+            if (fieldType == typeToSkip ||
+                (fieldType.IsArray & fieldType.GetElementType() == typeToSkip))
+                return true;
+            else return false;
+        }
+
         public static List<T2> GenerateListForeachField<T1,T2>(T1 structure, Action<List<T2>,System.Reflection.FieldInfo> action)
         {
             var fields = structure.GetType().GetFields();
             var returnVal = new List<T2>(fields.Length);
             foreach (var field in fields)
+            {
+                if (ShouldSkip(field.FieldType, typeof(Unknown)) || 
+                    ShouldSkip(field.FieldType, typeof(Padding)))
+                    continue;
                 action(returnVal, field);
+            }
+                
             return returnVal;
         }
 
@@ -64,10 +88,29 @@ namespace DarkSouls3ExtractionTool
             });
         }
 
+        public static List<string> GetFieldOffsets<T>(T structure)
+        {
+            return GenerateListForeachField<T, string>(structure, (x, y) =>
+            {
+                if (y.FieldType.IsArray)
+                {
+                    var baseOffset = Marshal.OffsetOf(typeof(T), y.Name).ToInt32();
+                    var array = (Array)y.GetValue(structure);
+                    var length = array.Length;
+                    var sizeOfArrayItem = Marshal.SizeOf(array.GetValue(0).GetType());
+                    for (var i = 0; i < length; i++)
+                        x.Add((baseOffset + i * sizeOfArrayItem).ToString("X"));
+                }
+                else x.Add(Marshal.OffsetOf(typeof(T), y.Name).ToString("X"));
+            });
+        }
+
         public static void TryExportParam<T>(string filename) where T : struct
         {
             if (!File.Exists(filename))
                 return;
+
+            Console.WriteLine($"Exporting {filename}...");
 
             var binaryData = File.ReadAllBytes(filename);
             var length = BitConverter.ToInt16(binaryData, 0xA);
@@ -75,21 +118,30 @@ namespace DarkSouls3ExtractionTool
             var fname = Path.GetFileNameWithoutExtension(filename);
             var exportFile = File.CreateText($"{fname}Out.csv");
 
-            if (headers)
+            if ((arguments & Arguments.Headers) == Arguments.Headers ||
+                (arguments & Arguments.Debug) == Arguments.Debug)
             {
-                var fieldNames = GetFieldNames(BytesToStruct<T>(binaryData, (int)baseOffset));
-                exportFile.WriteLine("{0},{1},{2}", "id", "index", string.Join(",", fieldNames));
+                var firstStruct = BytesToStruct<T>(binaryData, (int)baseOffset);
+                var fieldNames = GetFieldNames(firstStruct);
+                if ((arguments & Arguments.HeadersPlus) == Arguments.HeadersPlus ||
+                    (arguments & Arguments.Debug) == Arguments.Debug)
+                {
+                    var fieldOffsets = GetFieldOffsets(firstStruct);
+                    exportFile.WriteLine(",,,{0}", string.Join(",", fieldOffsets));
+                }
+                exportFile.WriteLine("{0},{1},{2},{3}", "id", "index", "offset", string.Join(",", fieldNames));
             }
 
             for (var i = 0; i < length; i++)
             {
                 var offset = BitConverter.ToUInt32(binaryData, 0x30 + 0x18 * i);
+                var realOffset = (int)baseOffset + Marshal.SizeOf(typeof(T)) * i;
                 var id = BitConverter.ToUInt32(binaryData, 0x40 + 0x18 * i);
-                Console.WriteLine($"ID: {id}, INDEX: {i}, OFFSET: {offset}");
-                var structure = BytesToStruct<T>(binaryData, (int)baseOffset + Marshal.SizeOf(typeof(T)) * i);
 
+                var structure = BytesToStruct<T>(binaryData, realOffset);
                 var fieldValues = GetFieldValues(structure);
-                exportFile.WriteLine("{0},{1},{2}", id, i, string.Join(",", fieldValues));
+
+                exportFile.WriteLine("{0},{1},{2},{3}", id, i, realOffset, string.Join(",", fieldValues));
             }
             exportFile.Close();
         }
@@ -106,15 +158,73 @@ namespace DarkSouls3ExtractionTool
                     case "-h":
                         Console.WriteLine("To use this tool simply place .param files from the game in the same folder and run it.");
                         Console.WriteLine("Arguments:");
-                        Console.WriteLine("-h\tHelp");
-                        Console.WriteLine("-H\tOutput column headers");
+                        Console.WriteLine("-h\tHelp.");
+                        Console.WriteLine("-H\tOutput column headers.");
+                        Console.WriteLine("-H+\tOutput structure offsets and headers.");
+                        Console.WriteLine("-d\tDebug Mode, outputs hidden columns.");
                         Environment.Exit(0);
                         break;
                     case "-H":
-                        headers = true;
+                        arguments |= Arguments.Headers;
+                        break;
+                    case "-H+":
+                        arguments |= Arguments.HeadersPlus;
+                        break;
+                    case "-d":
+                        arguments |= Arguments.Debug;
                         break;
                 }
             }
+        }
+
+        static string ReadNullTerminatedWideString(byte[] data, int offset, int maxLength)
+        {
+            List<byte> charList = new List<byte>(maxLength);
+            for (var i = 0; i < maxLength; i+=2)
+            {
+                if (offset + i + 1 >= data.Length)
+                    break;
+                var a = data[offset + i];
+                var b = data[offset + i + 1];
+                charList.Add(a);
+                charList.Add(b);
+                if (a == 0x0 & b == 0x0)
+                    break;
+            }
+            return System.Text.Encoding.Unicode.GetString(charList.ToArray());
+        }
+
+        static void TryExportFmg(string filename)
+        {
+            if (!File.Exists(filename))
+                return;
+
+            Console.WriteLine($"Exporting {filename}...");
+
+            var binaryData = File.ReadAllBytes(filename);
+            var stringIndexTableBaseOffset = BitConverter.ToInt32(binaryData, 0x18);
+            var length = BitConverter.ToInt32(binaryData, 0x0C);
+            var fname = Path.GetFileNameWithoutExtension(filename);
+            var exportFile = File.CreateText($"{fname}Out.csv");
+
+            for (var i = 0; i < length; i++)
+            {
+                var IdLow = BitConverter.ToInt32(binaryData, 0x2C + i * 16);
+                var IdHigh = BitConverter.ToInt32(binaryData, 0x30 + i * 16);
+                var offset = BitConverter.ToInt32(binaryData, 0x28 + i * 16);
+                for (var j = IdLow; j <= IdHigh; j++)
+                {
+                    var index = j - IdLow + offset;
+                    var tableOffset = BitConverter.ToInt32(binaryData, stringIndexTableBaseOffset + index * 8);
+                    if (tableOffset != 0)
+                    {
+                        var str = ReadNullTerminatedWideString(binaryData, tableOffset, 200);
+                        exportFile.WriteLine("{0},{1},{2},{3},{4}", IdLow, IdHigh, j, tableOffset, str);
+                    }
+                    
+                }
+            }
+            exportFile.Close();
         }
 
         static void Initialize()
@@ -130,11 +240,17 @@ namespace DarkSouls3ExtractionTool
             Initialize();
             ParseArgs(args);
 
-            TryExportParam<CalcCorrectGraph>("CalcCorrectGraph.param");
+            //TryExportFmg("武器名.fmg"); // Weapon Names
+            //TryExportFmg("NPC名.fmg");
             TryExportParam<EquipParamWeapon>("EquipParamWeapon.param");
+            TryExportParam<EquipParamAccesory>("EquipParamAccessory.param");
+            TryExportParam<EquipParamProtector>("EquipParamProtector.param");
             TryExportParam<ReinforceParamWeapon>("ReinforceParamWeapon.param");
             TryExportParam<AttackElementCorrectParam>("AttackElementCorrectParam.param");
+            TryExportParam<CalcCorrectGraph>("CalcCorrectGraph.param");
+            TryExportParam<Magic>("Magic.param");
             TryExportParam<SpEffectParam>("SpEffectParam.param");
+            TryExportParam<NpcParam>("NpcParam.param");
         }
     }
 }
